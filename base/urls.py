fc8a87e61699b564a804e8e3d3096fbc6e1a60b1
Andreh-Z <witecan931@gmail.com>
2023-02-09 13:56:22 -0300

    search_by_id

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,10 @@
 # Importar la función "path" y el módulo de vistas "views" desde Django
 from django.urls import path
-from .views import TaskList
+from .views import TaskList, TaskDetail
 
 # Crear una lista de patrones de URL
 urlpatterns = [
     # Especificar la URL raíz del sitio web (con ""), que activará la vista "taskList"
      path('', TaskList.as_view(), name='tasks'),
+     path("task/<int:pk>/", TaskDetail.as_view(), name="task")
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 # Create your views here.
 
@@ -8,3 +9,7 @@ from .models import Task
 class TaskList(ListView):
        model = Task
        context_object_name = "Tasks"
+
+
+class TaskDetail(DetailView):
+       model = Task #Model Name
